b6f8714e9095150d62eb6e363ed0a9100362b448
Joey <joey@xm8.ninja>
2020-06-08 11:34:33 -0400

    done MVP

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,29 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
+//remember async
+//arrange, act, assert, boom
+test("form header renders", async () => {
+  // arrange it
+  const { getByText } = render(<CheckoutForm />);
+  // act it out
+  const header = getByText(/checkout form/i);
+  // assert it
+  expect(header).toBeInTheDocument();
+  // boom
+});
 
-test("form header renders", () => {});
-
-test("form shows success message on submit with form details", () => {});
+//remember async
+//arrange, act, assert, boom
+test("form shows success message on submit with form details", async () => {
+  // arrange it
+  const { getByTestId } = render(<CheckoutForm />);
+  // act it out
+  const btnTest = getByTestId("check");
+  fireEvent.click(btnTest);
+  // assert
+  expect(btnTest).toBeInTheDocument;
+  //boom
+});
