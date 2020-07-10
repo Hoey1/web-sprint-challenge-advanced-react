9e8867f5319e2e8564861647048f9310f9457bdb
Joey <joey@xm8.ninja>
2020-07-10 12:57:51 -0400

    also trying the shopping cart test stretch

---
+++ client/src/components/ShoppingCart.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ShoppingCart from './ShoppingCart';
+import { render, fireEvent } from '@testing-library/react';
+import PlantList from './PlantList';
+
+
+// fireEvent button to add plant to cart
+// view cart and confirm plant is in the cart
+
+
+const plants = {{}, {}, {}} // with each object being a mock plant
+test("displays plants in cart", () => {
+  const { getByText } = render(<ShoppingCart cart={plants} />)
+
+  ...
+
+})
