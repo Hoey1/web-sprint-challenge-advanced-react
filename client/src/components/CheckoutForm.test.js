5c305a3a4f9e97036b380f0decfa0c7bce8eb05c
Joey <joey@xm8.ninja>
2020-07-10 18:48:24 -0400

    fixed error with CheckoutForm.test.js after 1:1 - all tests are passing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  getByLabelText,
+  getByTestId,
+} from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -15,12 +20,42 @@ test("form header renders", async () => {
 });
 
 test("form shows success message on submit with form details", async () => {
-  // Arrange
-  const { getByTestId } = render(<CheckoutForm />);
-  // Act
-  const btnTest = getByTestId("check");
-  fireEvent.click(btnTest);
-  // Assert
-  expect(btnTest).toBeInTheDocument;
-  // 💣💣
+  const { getByLabelText, getByTestId, getAllByText } = render(
+    <CheckoutForm />
+  );
+
+  fireEvent.change(getByLabelText(/first name/i), {
+    target: { value: "Joey" },
+  });
+
+  fireEvent.change(getByLabelText(/last name/i), {
+    target: { value: "Wilson" },
+  });
+
+  fireEvent.change(getByLabelText(/address/i), {
+    target: { value: "1801 Kalarama St NW" },
+  });
+
+  fireEvent.change(getByLabelText(/city/i), {
+    target: { value: "Washington" },
+  });
+
+  fireEvent.change(getByLabelText(/state/i), {
+    target: { value: "DC" },
+  });
+
+  fireEvent.change(getByLabelText(/zip/i), {
+    target: { value: "20012" },
+  });
+
+  fireEvent.click(getAllByText(/checkout/i)[1]);
+
+  getByTestId("successMessage");
 });
+
+// // Act
+// const btnTest = getByTestId("check");
+// fireEvent.click(btnTest);
+// // Assert
+// expect(btnTest).toBeInTheDocument;
+// // 💣💣
